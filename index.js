beb93977ae5bd4f5eb806443ff6384893ab75e77
fman-2003 <olatunjifulfilment95@gmail.com>
2021-04-22 12:25:23 -0700

    my first push to git

---
+++ index.js
@@ -0,0 +1,91 @@
+// JAVASCRIPT DATA TYPES
+// tHERER ARE two main types of data
+
+// 1.) PRIMITIVE DATA TYPES
+// 2.) NON-PRIMITIVE DATA TYPES
+
+// PRIMITIVE DATA TYPES
+// 1. NUMBER
+// 2. STRINGS
+// 3. BOOLEAN
+// 4. UNDEFINED
+// 5. NULL
+
+//EXAMPES
+
+const age = 32 //number
+//const name = 'Testimony njgjkdadjeJEJIGEuj' //string
+const isMarried = false // boolean
+const numOfChildren = undefined;
+const pond = null;
+
+// let age = 32 //number
+// let name = 'Testimony njgjkdadjeJEJIGEuj' //string
+// let isMarried = false // boolean
+// let numOfChildren = undefined;
+// let pond = null;
+
+// var age = 32 //number
+// var name = 'Testimony njgjkdadjeJEJIGEuj' //string
+// var isMarried = false // boolean
+// var numOfChildren = undefined;
+// var pond = null;
+
+// assignment
+
+//create 20 variables 4 each for each of the primitive data types
+
+
+// 22/4/2021
+// VARIABLE
+
+//var name = "Ola"; //first method of creating a variable
+
+
+//let age = 10  //second method of creating a variable
+
+const isMale = true; // third way of creating a variable
+
+let item1 = {
+    name: "Infinx",
+    model: "Note3 plus",
+    price: "$1200",
+    used: false
+}
+
+const pc = {
+    maker: "hp",
+    used: true,
+    model: "elite-book pro"
+}
+
+console.log(pc)
+
+// primitive data TYPES AND THEIR METHODS
+
+//1.) STRINGS
+
+let word = "Latitude";
+
+const uppercaseWord = word.toUpperCase()
+const lowercaseWord = word.toLowerCase()
+const reversedWord = word.reverse()
+const lengthOfWords = word.length
+
+
+// Assigment
+
+const userName = "Aalole";
+// find ten string methods and use them on the userName
+
+// Number
+
+const price = 20.614
+
+// BOOLEAN
+
+const lightOn = false;
+
+const name = null;
+
+let date;
